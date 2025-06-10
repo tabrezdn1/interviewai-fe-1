a5b929f0a02ef7070e28c540f509de7a430cb6f4
Tabrez Shaik <37013844+tabrezdn1@users.noreply.github.com>
2025-06-09 22:52:20 -0400

    Updated InterviewSetup.tsx

--- src/pages/InterviewSetup.tsx
+++ src/pages/InterviewSetup.tsx
@@ -753,7 +753,7 @@ const InterviewSetup: React.FC = () => {
                                   
                                   <div className="flex items-center gap-3">
                                     {round.isSelected && (round.count || 1) > 1 && (
-                                      <Badge variant="outline" className="bg-blue-50">
+                                      <Badge variant="outline\" className="bg-blue-50">
                                         {round.count}x
                                       </Badge>
                                     )}
